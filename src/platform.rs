008a557c98a3f1f38c532ea81a250b5b5a83ad68
viri <hi@viri.moe>
2021-03-18 01:40:00 -0600

    platform: unnecessary doc tags
    These won't ever actually render, so...

--- src/platform.rs
+++ src/platform.rs
@@ -13,6 +13,5 @@
 #[cfg_attr(feature = "nightly-docs", doc(cfg(target_os = "windows")))]
 #[cfg_attr(not(feature = "nightly-docs"), cfg(target_os = "windows"))]
 pub mod win32;
-#[cfg_attr(feature = "nightly-docs", doc(cfg(target_os = "windows")))]
-#[cfg_attr(not(feature = "nightly-docs"), cfg(target_os = "windows"))]
+#[cfg(target_os = "windows")]
 pub(crate) use win32 as imp;
