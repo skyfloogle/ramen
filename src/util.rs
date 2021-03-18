756bc3e78befc77b08e5dd184c94d8030550e6d9
viri <hi@viri.moe>
2021-03-18 14:47:12 -0600

    util: add & impl `LazyCell` type

--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,61 @@
-use std::{clone::Clone, sync::Arc};
+use std::{
+    cell::UnsafeCell,
+    clone::Clone,
+    ops::Deref, ptr,
+    sync::{Arc, Once},
+};
 
+/// Minimal lazily initialized type, similar to the one in `once_cell`.
+///
+/// Thread safe initialization, immutable-only access.
+pub struct LazyCell<T, F = fn() -> T> {
+    // Invariant: Written to at most once on first access.
+    init: UnsafeCell<Option<F>>,
+    ptr: UnsafeCell<*const T>,
+
+    // Synchronization primitive for initializing `init` and `ptr`.
+    once: Once,
+}
+
+unsafe impl<T, F> Send for LazyCell<T, F> where T: Send {}
+unsafe impl<T, F> Sync for LazyCell<T, F> where T: Sync {}
+
+impl<T, F> LazyCell<T, F> {
+    pub const fn new(init: F) -> Self {
+        Self {
+            init: UnsafeCell::new(Some(init)),
+            ptr: UnsafeCell::new(ptr::null()),
+            once: Once::new(),
+        }
+    }
+}
+
+impl<T, F: FnOnce() -> T> LazyCell<T, F> {
+    pub fn get(&self) -> &T {
+        self.once.call_once(|| unsafe {
+            if let Some(f) = (&mut *self.init.get()).take() {
+                let pointer = Box::into_raw(Box::new(f()));
+                ptr::write(self.ptr.get(), pointer);
+            }
+        });
+
+        // SAFETY: A call to `call_once` initialized the pointer
+        unsafe {
+            &**self.ptr.get()
+        }
+    }
+}
+
+impl<T, F: FnOnce() -> T> Deref for LazyCell<T, F> {
+    type Target = T;
+
+    #[inline]
+    fn deref(&self) -> &Self::Target {
+        self.get()
+    }
+}
+
+/// Static or dynamic data shared across threads.
 pub enum MaybeArc<T: 'static + ?Sized> {
     Static(&'static T),
     Dynamic(Arc<T>),
