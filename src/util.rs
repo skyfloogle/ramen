8f45652fce51ec187c1074e137bcb0b0463e0cbf
viri <hi@viri.moe>
2021-03-18 02:07:42 -0600

    util: impl Clone for `MaybeArc`

--- src/util.rs
+++ src/util.rs
@@ -1,15 +1,24 @@
-use std::sync::Arc;
+use std::{clone::Clone, sync::Arc};
 
 pub enum MaybeArc<T: 'static + ?Sized> {
     Static(&'static T),
     Dynamic(Arc<T>),
 }
 
-impl<T> AsRef<T> for MaybeArc<T> {
+impl<T: 'static + ?Sized> AsRef<T> for MaybeArc<T> {
     fn as_ref(&self) -> &T {
         match self {
             Self::Static(s) => s,
             Self::Dynamic(d) => d.as_ref(),
         }
     }
+}
+
+impl<T: 'static + ?Sized> Clone for MaybeArc<T> {
+    fn clone(&self) -> Self {
+        match self {
+            Self::Static(x) => Self::Static(x),
+            Self::Dynamic(x) => Self::Dynamic(Arc::clone(x)),
+        }
+    }   
 }
