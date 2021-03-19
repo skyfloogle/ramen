708e5cf46d47ce04688143abda2618571aa58c03
viri <hi@viri.moe>
2021-03-18 19:03:52 -0600

    window: add `Style` struct, *many* builder flags

--- src/window.rs
+++ src/window.rs
@@ -50,6 +50,7 @@ pub struct Window(imp::WindowRepr);
 #[derive(Clone)]
 pub struct WindowBuilder {
     pub(crate) class_name: MaybeArc<str>,
+    pub(crate) style: Style,
     pub(crate) title: MaybeArc<str>,
 }
 
@@ -63,6 +64,16 @@ impl WindowBuilder {
     pub(crate) const fn new() -> Self {
         Self {
             class_name: MaybeArc::Static("ramen_window"),
+            style: Style {
+                borderless: false,
+                resizable: true,
+                visible: true,
+                controls: Some(Controls::enabled()),
+                rtl_layout: false,
+
+                #[cfg(windows)]
+                tool_window: false,
+            },
             title: MaybeArc::Static("a nice window"),
         }
     }
@@ -73,6 +84,22 @@ impl WindowBuilder {
 }
 
 impl WindowBuilder {
+    /// Sets whether the window is initially without a border.
+    ///
+    /// Defaults to `false`.
+    #[inline]
+    pub fn borderless(&mut self, borderless: bool) -> &mut Self {
+        self.style.borderless = borderless;
+        self
+    }
+
+    /// Sets the platform-specific window class name.
+    ///
+    /// - Win32: `lpszClassName` in
+    /// [`WNDCLASSEXW`](https://docs.microsoft.com/en-us/windows/win32/api/winuser/ns-winuser-wndclassexw)
+    /// - TODO: Other platforms!
+    ///
+    /// Defaults to `"ramen_window"`.
     pub fn class_name<T>(&mut self, class_name: T) -> &mut Self
     where
         T: Into<Cow<'static, str>>,
@@ -81,6 +108,34 @@ impl WindowBuilder {
             Cow::Borrowed(b) => MaybeArc::Static(b),
             Cow::Owned(o) => MaybeArc::Dynamic(o.into()),
         };
+        self
+    }
+
+    /// Sets the initial window controls.
+    /// `None` indicates that no control menu is desired.
+    ///
+    /// Defaults to [`Controls::enabled`].
+    #[inline]
+    pub fn controls(&mut self, controls: Option<Controls>) -> &mut Self {
+        self.style.controls = controls;
+        self
+    }
+
+    /// Sets whether the window is initially resizable.
+    ///
+    /// Defaults to `true`.
+    #[inline]
+    pub fn resizable(&mut self, resizable: bool) -> &mut Self {
+        self.style.resizable = resizable;
+        self
+    }
+
+    /// Sets whether the window controls and titlebar have a right-to-left layout.
+    ///
+    /// Defaults to `false`.
+    #[inline]
+    pub fn rtl_layout(&mut self, rtl_layout: bool) -> &mut Self {
+        self.style.rtl_layout = rtl_layout;
         self
     }
 
@@ -107,4 +162,25 @@ impl WindowBuilder {
         };
         self
     }
+
+    /// Sets whether the window is initially visible.
+    ///
+    /// Defaults to `true`.
+    #[inline]
+    pub fn visible(&mut self, visible: bool) -> &mut Self {
+        self.style.visible = visible;
+        self
+    }
+}
+
+#[derive(Clone)]
+pub(crate) struct Style {
+    pub borderless: bool,
+    pub resizable: bool,
+    pub visible: bool,
+    pub controls: Option<Controls>,
+    pub rtl_layout: bool,
+
+    #[cfg(windows)]
+    pub tool_window: bool,
 }
