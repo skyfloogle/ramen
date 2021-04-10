830337b5bb598e6ab12b855d742152ce65624709
viri <hi@viri.moe>
2021-04-10 01:31:14 -0600

    root: add `input` module

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,10 @@
 #[macro_use]
 pub(crate) mod util;
 
+#[cfg_attr(feature = "nightly-docs", doc(cfg(feature = "input")))]
+#[cfg_attr(not(feature = "nightly-docs"), cfg(feature = "input"))]
+pub mod input;
+
 pub mod error;
 pub mod event;
 pub mod monitor;
