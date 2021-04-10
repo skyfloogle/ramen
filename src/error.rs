5a44abd953dba8392e87a69037bdca38bd89780f
viri <hi@viri.moe>
2021-04-10 02:11:04 -0600

    error: `std::error::Error` stub

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,13 @@
+use std::{fmt, error};
+
 #[derive(Debug)]
 pub enum Error {
 
+}
+
+impl error::Error for Error {}
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "TODO") // TODO: !
+    }
 }
